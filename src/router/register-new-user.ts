6eac0b407e2af07f52ec5423a6fa28807decf80b
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-08-01 18:58:09 -0300

    create router to register address user

---
+++ src/router/register-new-user.ts
@@ -0,0 +1,37 @@
+import { Express, Request, Response } from 'express'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma.js';
+
+const FormUserZodSchema = z.object({
+    cep: z.coerce.string().min(8),
+    street: z.string().min(4),
+    number: z.coerce.number().min(1),
+    complement: z.string().min(4),
+    neighborhood: z.string().min(4),
+    city: z.string().min(4),
+    uf: z.string().min(2),
+});
+
+export async function createNewUserRegister(app: Express) {
+    app.post('/user/register', async (request: Request, response: Response) => {
+        const { cep, city, complement, neighborhood, number, street, uf } = FormUserZodSchema.parse(request.body)
+
+        const addressUser = await prisma.addressUser.create({
+            data: {
+                cep,
+                city,
+                complement,
+                neighborhood,
+                number,
+                street,
+                uf
+            }
+        })
+
+        if (!addressUser) {
+            throw new Error('AddressUser not found')
+        }
+
+        response.send({ addressUserId: addressUser.id })
+    })
+}
