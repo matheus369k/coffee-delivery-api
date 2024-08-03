b71fc2d9e295c2f6a1a92860134175e9944009cd
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-08-03 17:36:00 -0300

    config cors

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,11 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: '*',
+    }),
+);
 
 registerCoffee(app);
 getAllCoffees(app);
